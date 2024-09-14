2326d59867e4ad074c9ea71f42ac842b6fbe1bed
jcfaderanga <jcfaderanga@gmail.com>
2024-09-14 09:14:36 +0800

    create stuggle button file

---
+++ components/toggleBtn.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+
+const toggleBtn = () => {
+  return (
+    <View>
+      <TouchableOpacity>
+        
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default toggleBtn
+
+const styles = StyleSheet.create({})
