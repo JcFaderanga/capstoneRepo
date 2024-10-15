f589d43cfb01c21364d2367d794ffddc72acd5c5
jcfaderanga <jcfaderanga@gmail.com>
2024-10-15 13:50:58 +0800

    update dashboard UI/UX

--- components/donationDrive.jsx
+++ components/donationDrive.jsx
@@ -1,31 +1,47 @@
-import { ScrollView, View, Image, Text, TouchableOpacity, Dimensions } from 'react-native';
+import { ScrollView, View, Image, Text, TouchableOpacity, Dimensions, Pressable } from 'react-native';
 import React, { useState } from 'react';
 import { homeIcons } from '../constant';
-
+import Elevated from './elevated';
 
 const DonationDrive = ({location, time, date}) => {
   return (
-    <View className="w-[345px] h-[70] rounded-[20px] bg-white border border-[#CCCCCC] m-2 flex-column mx-auto" >
-      <View className="flex-1">
-        <View className="flex-1">
-          <View className=" flex-row py-2 px-5">
-            <Image style={{tintColor: '#F42F47'}} source={homeIcons.target} className="w-[18px] h-[21px]" />
-            <Text className="ml-3 flex-1">{location}</Text>
-          </View>
-          <View className="flex-row">
-              <View className="flex-1 flex-row  px-5 ">
-                <Image style={{tintColor: '#F42F47'}} source={homeIcons.clock} className="w-[20px] h-[20px]" />
-                <Text className="ml-3 flex-1 ">{time}</Text>
-              </View>
-              <View className="flex-1 flex-row  px-5 ">
-                <Image style={{tintColor: '#F42F47'}} source={homeIcons.calendar} className="w-[19px] h-[21px]" />
-                <Text className="ml-3 flex-1 ">{date}</Text>
-              </View>
-          </View>
-          
-        </View>
+    <>
+    <View className="w-full px-4">
+         <Elevated width={'100%'} height={"auto"} elevated={2}>
+            <View className="">
+                <View className="w-full max-h-28">
+                    <View className=" pt-2 ">
+                        <View className=" h-auto px-2 py-1 flex-row items-center">
+                            <Image source={homeIcons.target} className="w-10 h-10 rounded-full mx-3" resizeMode='contain'/>
+                              <View className="relative flex-1">
+                                  <Text className="text-[14px] font-bold flex-wrap">Festival Mall, Muntilupa City, 2nd floor 2nd floor </Text>
+                                  <Text className="text-[12px] ">Posted 1 hour ago</Text>
+                              </View>
+                        </View>       
+                    </View>
+                </View>
+            <View className=" border-b-2 border-gray-100">
+                <Text className="my-2 mx-4">Are you nearby? Join us and donate, be a hero and help save lives!</Text>
+            </View>        
+                <View className=" flex-row justify-between items-center px-4 py-3">
+                <Pressable className="rounded-[20px] rounded-bl-none w-28 h-9 bg-primaryRed flex-row justify-center items-center">
+                        <Text className="text-white font-bold px-2">See all</Text>
+                        <Image style={{tintColor: "white"}} source={require('../assets/icon/arrowNoCircle.png')} resizeMode='contain' className="w-4"/>
+                    </Pressable>
+                    <View className=" flex-row items-center h-10 pl-2">
+                              <Image style={{tintColor: '#f42f47'  }} source={homeIcons.calendar} className="w-6" resizeMode='contain'/>
+                                <View className="px-2">
+                                    <Text className="text-[16px] ">June 18, 2024 </Text>
+                                    <Text className="text-[12px] ">10am - 5pm</Text>
+                                </View>
+                          
+                    </View>
+                    
+                </View>
+            </View>
+        </Elevated>
       </View>
-    </View>
+    </>
   );
 };
 
