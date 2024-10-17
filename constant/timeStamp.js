945f283fe6d04c83654a8235e92eb90a7d5232c5
jcfaderanga <jcfaderanga@gmail.com>
2024-10-17 13:49:15 +0800

    update time stamp

--- constant/timeStamp.js
+++ constant/timeStamp.js
@@ -1,15 +1,22 @@
 export const TimeAgo = (timestamp) => {
-const time = [60, 3600, 86400, 604800, 2592000, 31536000];
-const date = ['minute', 'hour', 'day', 'week', 'month', 'year'];
-const now = new Date();
-const postDate = new Date(timestamp);
-const seconds = Math.floor((now - postDate) / 1000);
-for (let i = time.length - 1; i >= 0; i--) { 
-    const interval = Math.floor(seconds / time[i]);
-    if (interval >= 1) {
-        return(`${interval} ${date[i]}${interval === 1 ? '' : 's'} ago`);
-        break;
-    }
-}
+  const time = [31536000, 2592000, 604800, 86400, 3600, 60];
+  const date = ['year', 'month', 'week', 'day', 'hour', 'minute'];
+  const now = new Date();
+  const postDate = new Date(timestamp);
+  const seconds = Math.floor((now - postDate) / 1000);
+
+  for (let i = 0; i < time.length; i++) {
+      const interval = Math.floor(seconds / time[i]);
+      if (interval >= 1) {
+          return `${interval} ${date[i]}${interval === 1 ? '' : 's'} ago`;
+      }
+  }
+  return 'just now';
 };
+
+// 1year (365 days x 24 hours x 60 minutes x 60 seconds)
+// 1 month (30 days x 24 hours x 60 minutes x 60 seconds)
+// 1 week (7 days x 24 hours x 60 minutes x 60 seconds)
+// 1 day (24 hours x 60 minutes x 60 seconds)
+// 1hour (60 minutes x 60 seconds)
 
