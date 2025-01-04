7a9545eb7d7d5dd4c9b2af52983c31723f8675f7
jcfaderanga <jcfaderanga@gmail.com>
2025-01-04 13:58:32 +0800

    midify contentTitleBox or button for fax

--- components/contentTitle.jsx
+++ components/contentTitle.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 
 const ContentTitleButton = ({title, icon,backIcon, onPress, size}) => {
   return (
-    <TouchableOpacity className=" flex-row justify-between items-center w-full h-16 border-b-2 border-[#F2F2F2]"
+    <TouchableOpacity className=" flex-row justify-between items-center w-full h-20 border-b-2 border-[#F2F2F2] bg-white"
             onPress={onPress}>      
             <View className="flex-row justify-between items-center">
                 <View className="w-[50px] h-20 flex justify-center items-end">
                     <Image source={icon} resizeMode='contain' style={size}/>
                 </View>
-                <Text className="font-bold text-base ml-5">
+                <Text className="font-bold text-lg ml-5">
                     {title}
                 </Text>
             </View>  
             <View className="w-20 h-full flex justify-center items-center">
-                <Image source={backIcon} className="h-4 w-5" resizeMode='contain'/>
+                <Image source={backIcon} className="h-5 w-6" resizeMode='contain'/>
             </View>
     </TouchableOpacity>
   )
