f3b7c00aebe367ece7db9b483f8ed5ebb584b007
jcfaderanga <jcfaderanga@gmail.com>
2025-01-16 00:34:05 +0800

    fixed unit donated and next donation date, only show unit once the status us complete

--- hooks/blood_donation/fetchNextDonationDays.jsx
+++ hooks/blood_donation/fetchNextDonationDays.jsx
@@ -1,41 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
-import { supabase } from '../../lib/supabase';
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { supabase } from "../../lib/supabase";
 
 const UseFetchNextDonation = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [nextDonation, setNextDonation] = useState('--');
+  const [nextDonation, setNextDonation] = useState("--");
 
   const FetchNextDonation = async (donor_id) => {
     setLoading(true);
     try {
       const { data, error } = await supabase
-        .from('blood_donation')
-        .select('created_at')
-        .eq('donor', donor_id)
-        .order('created_at', { ascending: false }) 
+        .from("blood_donation")
+        .select("created_at")
+        .eq("donor", donor_id)
+        .eq("status", "complete")
+        .order("created_at", { ascending: false })
         .limit(1);
 
-        if (error) {
-            setError(error.message);
-          } else {
-            if (data.length > 0) {
-              // Get the most recent donation date
-              const lastDonationDate = new Date(data[0].created_at);
-              
-              // Add 56 days to the last donation date to get the next donation date
-              const nextDonation = new Date(lastDonationDate);
-              nextDonation.setDate(lastDonationDate.getDate() + 56); // Add 56 days
-    
-              // Format the next donation date
-              const formattedDate = nextDonation.toLocaleDateString(); // Format the date to a readable format
-              
-              setNextDonation(formattedDate);
-            } else {
-                setNextDonation('--'); // No donation history found, display '--'
-            }
-          }
+      if (error) {
+        setError(error.message);
+      } else {
+        if (data.length > 0) {
+          // Get the most recent donation date
+          const lastDonationDate = new Date(data[0].created_at);
+
+          // Add 56 days to the last donation date to get the next donation date
+          const nextDonation = new Date(lastDonationDate);
+          nextDonation.setDate(lastDonationDate.getDate() + 56); // Add 56 days
+
+          // Format the next donation date
+          const formattedDate = nextDonation.toLocaleDateString(); // Format the date to a readable format
+
+          setNextDonation(formattedDate);
+        } else {
+          setNextDonation("--"); // No donation history found, display '--'
+        }
+      }
     } catch (e) {
       setError(e.message);
     } finally {
--- hooks/blood_donation/fetchUnitDonated.jsx
+++ hooks/blood_donation/fetchUnitDonated.jsx
@@ -1,26 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
-import { supabase } from '../../lib/supabase';
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { supabase } from "../../lib/supabase";
 
 const UseFetchDonationCount = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [totalUnitDonated, setTotalUnitDonated] = useState(0); 
+  const [totalUnitDonated, setTotalUnitDonated] = useState(0);
 
   const FetchUnitCount = async (user_id) => {
     setLoading(true);
     try {
       const { data, error } = await supabase
-        .from('blood_donation')
-        .select('units_donated')
-        .eq('donor', user_id);
+        .from("blood_donation")
+        .select("units_donated")
+        .eq("donor", user_id)
+        .eq("status", "complete");
 
       if (error) {
         setError(error.message);
       } else {
-
-        const total = data.reduce((sum, item) => sum + (item.units_donated || 0), 0);
-        setTotalUnitDonated(total); 
+        const total = data.reduce(
+          (sum, item) => sum + (item.units_donated || 0),
+          0
+        );
+        setTotalUnitDonated(total);
       }
     } catch (e) {
       setError(e.message);
